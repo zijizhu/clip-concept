1587f5685c13289d1ec828a94fa2fedce01809d6
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-05-03 16:33:26 +1000

    update setup script

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 set -x
 
 mkdir -p datasets/CUB && cd datasets/CUB
-wget
+wget -O CUB_200_2011.tgz -q --show-progress "https://www.dropbox.com/scl/fi/ceqpnwgww8xsn0h2guca6/CUB_200_2011.tgz?rlkey=6dr3xhhrelp9vos0qyepprwcr&st=zuvyya6w&dl=0"
 tar -zxf CUB_200_2011.tgz CUB_200_2011
